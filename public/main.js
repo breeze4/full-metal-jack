c1fbc9b55c8d51adb38a36541d646ac872100aac
Matt Bailey <ambiturner0@gmail.com>
2025-02-09 16:15:32 -0800

    clean up elements and styles

--- public/main.js
+++ public/main.js
@@ -7,7 +7,7 @@ import { Map } from './map.js';
 import { GameEngine } from './gameEngine.js';
 
 const storageManager = new StorageManager();
-const map = new Map(1000, 400); // Define a map of size 1000x1000
+const map = new Map(1000, 800); // Define a map of size 1000x1000
 const gameEngine = new GameEngine(SoldierConfig, WeaponConfig, GameModeConfig, GameStats, map);
 
 // Bind UI elements
